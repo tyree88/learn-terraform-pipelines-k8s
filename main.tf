ab3a455543d88c0c59a08d41fee9830ba44b3cdc
tyree <tyreepearson88@gmail.com>
2021-09-23 13:41:02 -0500

    changed the orginzation name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "tyreepearson"    
     workspaces {
       name = "learn-terraform-pipelines-k8s"
     }
